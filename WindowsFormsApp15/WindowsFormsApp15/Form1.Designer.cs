8f5d8854ebcae9d22cdbb71c7d1a90ebcf82ea3b
the Tap <97515458+Tapicka1984@users.noreply.github.com>
2022-04-25 09:40:22 +0200

    Add files via upload

---
+++ WindowsFormsApp15/WindowsFormsApp15/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(209, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "až zadáš větu, tak mě zmáčkni";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nejcastejsi pismeno:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Pocet vyskytů:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(122, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "tato pismena nejsou ve věte:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 165);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "X";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ WindowsFormsApp15/WindowsFormsApp15/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Text_od_uzivatele = textBox1.Text;
+            if(Text_od_uzivatele.Contains("  "))
+            {
+                Text_od_uzivatele.Replace("  "," ");
+            }
+            int pocitadlo_nejcastejsiho_znaku = 0;
+            int pismeno = 97;
+            int testovaci_promena = 0;
+            char pismeno2 = 'a';
+            char nejcastejsi_pismeno = ' ';
+            for(int i = 0; i < 26; i++)
+            {
+                foreach (char x in Text_od_uzivatele)
+                {
+                    if(x == pismeno)
+                    {
+                        pocitadlo_nejcastejsiho_znaku++;
+                        pismeno2 = x;
+                    }
+                }
+                if(pocitadlo_nejcastejsiho_znaku > testovaci_promena)
+                {
+                    testovaci_promena = pocitadlo_nejcastejsiho_znaku;
+                    nejcastejsi_pismeno = pismeno2;
+                }
+                pismeno++;
+                pocitadlo_nejcastejsiho_znaku = 0;
+            }
+            label2.Text = nejcastejsi_pismeno.ToString();
+            label4.Text = testovaci_promena.ToString();
+            //tohle funguje - nesahat; abecade - abcdefghijklmnopqrstuvwxyz;
+            char testovaci_pismeno = 'a';
+            char[] pole = new char[26];
+            int pismeno5 = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                foreach(char y in Text_od_uzivatele)
+                {
+                    if(y == testovaci_pismeno)
+                    {
+                        pole[pismeno5] = ' ';
+                    }
+                    else if (y != testovaci_pismeno)
+                    {
+                        pole[pismeno5] = testovaci_pismeno;
+                    }    
+                }
+                testovaci_pismeno++;
+                pismeno++;
+            }
+            label6.Text = new string(pole);
+        }
+    }
+}
